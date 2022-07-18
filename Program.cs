dc65d223a1710eef67d377323c6eace500f23785
Frieda <89573397+Frieda-VI@users.noreply.github.com>
2022-07-18 15:46:52 +0200

    Added complexity to Program.cs

--- Program.cs
+++ Program.cs
@@ -2,8 +2,18 @@
 
 class Program 
 {
+    public static string[] randomMessages = {
+        "Frieda is a programmer.",
+        "CSharp can be quite confusing.",
+        "Multi-dimensional arrays are useful!",
+        "The Earth is not flat."
+    };
+
     public static void Main(String[] Args)
     {
-        Console.WriteLine("Hello World!");
+        Random randomInteger = new Random();
+        int Integer = randomInteger.Next((int) randomMessages.Length);
+
+        Console.WriteLine("Your message is: " + randomMessages[Integer]);
     }
 }
