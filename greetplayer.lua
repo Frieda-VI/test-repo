c2cee072e5c5e7abfdb72c55d3bc0793baf0ecd4
Frieda <89573397+Frieda-VI@users.noreply.github.com>
2022-07-18 16:02:26 +0200

    Added player leaving feature

--- greetplayer.lua
+++ greetplayer.lua
@@ -3,3 +3,7 @@ local Players = game:GetService("Players")
 Players.PlayerAdded:Connect(function(player)
 	print("Hello World, " .. player.Name)
 end)
+
+Players.PlayerRemoving:Connect(function(player)
+	print(player.Name .. " left the game!")
+end)
